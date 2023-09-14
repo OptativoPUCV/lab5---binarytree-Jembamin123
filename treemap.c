857da1d8046a5df609457328f7783e428bb5b2ba
Replit user <>
2023-09-14 21:50:25 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -49,7 +49,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
   Pair* newPair=(Pair*)malloc(sizeof(Pair));
   newPair->key = key;
   newPair->value = value;
-  TreeNode* newNode = createTreeNode(newPair);
+  TreeNode* newNode = createTreeNode(newPair->key, newPair->value);
 
   TreeNode* currentNode = tree->root;
   TreeNode* parentNode = NULL;
