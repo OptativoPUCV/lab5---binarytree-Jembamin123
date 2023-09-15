75097ac67da879d4bb836845cb3a86f363d8f916
Replit user <>
2023-09-15 17:11:33 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -19,6 +19,11 @@ struct TreeMap {
     int (*lower_than) (void* key1, void* key2);
 };
 
+int is_equal(TreeMap* tree, void* key1, void* key2){
+    if(tree->lower_than(key1,key2)==0 &&  
+        tree->lower_than(key2,key1)==0) return 1;
+    else return 0;
+}
 
 
 TreeNode * createTreeNode(void* key, void * value) {
